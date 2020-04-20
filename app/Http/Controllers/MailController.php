df74b46d41fe97d8401fdcb67e3912cddf5de0c1
vaso369 <vasilije.vasilijevic.11.17@ict.edu.rs>
2020-04-20 14:33:10 +0200

    mail changes

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,16 +10,16 @@ class MailController extends Controller
 {
     public function sendMail(Request $request)
     {
-        $emailFrom = $request->input("email");
+        $email_from = $request->input("email");
         $message = $request->input("message");
 
-        $data = array('name' => "Virat Gandhi");
+        $to = 'vasilije.vasilijevic.11.17@ict.edu.rs';
+        $email_subject = "Task management contact form";
+        $email_body = "You have received a new message from your website contact form.Here are the details:\n\nEmail: $email_from\n\nMessage:\n$message";
+        $headers = "$email_from\n";
+        $headers .= "Reply-To: $email_from";
+        mail($to, $email_subject, $email_body, $headers);
 
-        Mail::send(['text' => 'mail'], $emailFrom, function ($message) {
-            $message->to('abc@gmail.com', 'Tutorials Point')->subject
-                ('Task management system support');
-            $message->from($emailFrom);
-        });
     }
 
 }
